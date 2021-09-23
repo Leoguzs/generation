2dabc45167977126753e7529a209a6d23012ac3b
Leonardo Guzmán <lguzs96@mail.com>
2021-09-23 11:15:30 -0500

    mas ejercicios con JS

---
+++ JS semana 5/12-ternario.js
@@ -0,0 +1,5 @@
+/* operador ternario */
+let edad = 10
+
+const mensaje = (edad>=18)  ? 'Eres  mayor edad' : 'eres menor';
+console.log(mensaje)
---
+++ JS semana 5/13-arrow-fuction.js
@@ -0,0 +1,6 @@
+
+
+let suma = (num1, num2) => {
+    return num1 + num2
+}
+console.log(suma(1,2))
