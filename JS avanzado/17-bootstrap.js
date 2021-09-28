eb3794023c1e0b70663641421f9b78abf9b51cf4
Leonardo Guzmán <lguzs96@mail.com>
2021-09-28 18:50:33 -0500

    DOM CARDS

---
+++ JS avanzado/17-bootstrap.js
@@ -0,0 +1,25 @@
+const $cardsContainer = document.querySelector('#cardsContainer')
+
+/* Voy a hacer una función que me permita hacer un ciclo */
+
+let mostrarCards = () =>{
+    fetch('https://jsonplaceholder.typicode.com/albums/1/photos')
+    .then(response => response.json()).then(json =>{
+        json.forEach(element => {
+            let col = document.createElement('div')
+            col.classList.add('col-md-4')
+            col.classList.add('col-sm-6') 
+           /*  col.classList.add('m-1')  */
+            col.innerHTML = `<div class="card" ">
+            <img src="${element.url}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">${element.title}</p>
+            </div>
+          </div>`
+          ;
+          $cardsContainer.appendChild(col)
+        });
+    })
+}
+
+mostrarCards()
